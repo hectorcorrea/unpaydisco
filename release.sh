b935badab085851dd220f7d3e5113f04fb215fb0
Hector Correa <hector_correa@brown.edu>
2019-03-07 12:34:06 -0500

    Linux only releases for now

--- release.sh
+++ release.sh
@@ -1,13 +1,11 @@
 echo "Creating Linux version..."
 GOOS=linux go build
-zip -r solrdora_linux.zip solrdora settings.json public/* views/*
+zip -r unpaydisco_linux.zip unpaydisco settings.json public/* views/* data/*
 
+# echo "Creating Windows version..."
+# GOOS=windows GOARCH=386 go build -o solrdora.exe
+# zip -r unpaydisco_win.zip unpaydisco.exe settings.json public/* views/* data/*
 
-echo "Creating Windows version..."
-GOOS=windows GOARCH=386 go build -o solrdora.exe
-zip -r solrdora_win.zip solrdora.exe settings.json public/* views/*
-
-
-echo "Creating Mac OS X version..."
-go build
-zip -r solrdora_mac.zip solrdora settings.json public/* views/*
+# echo "Creating Mac OS X version..."
+# go build
+# zip -r unpaydisco_mac.zip unpaydisco settings.json public/* views/* data/*
